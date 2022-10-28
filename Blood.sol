9531138c905a5f0653368d2b8c51c7ce0cebd325
sanika-phadke <113808867+sanika-phadke@users.noreply.github.com>
2022-10-28 09:36:01 +0530

    Update Blood.sol

--- Blood.sol
+++ Blood.sol
@@ -3,9 +3,17 @@ contract Blood{
     
         address whoDonated;
         uint donated;
-        enum
+        struct BloodGrps{
+            uint apos;
+            uint aneg;
+            uint bpos;
+            uint bneg;
+            uint opos;
+            uint oneg;
+        }
+        
     
-    mapping(address => bool) alreadyUser;
+    mapping(address => BloodGrps) bloodproj;
     /*function whoDonated() public{
         if(alreadyUser[msg.sender]==true)
         //function 
@@ -17,6 +25,26 @@ contract Blood{
     function Donate() public payable{
         donated=msg.value;
 
+    }
+    function availableBloodGrps(uint choice) public 
+    {
+          var group= bloodproj[msg.sender]; /*giving error*/
+         if(choice>=1 && choice<=6)
+         {
+             if(choice==1)
+             group.apos++;
+             else if(choice==2)
+             group.aneg++;
+             else if(choice==3)
+             group.bpos++;
+             else if(choice==4)
+             group.bneg++;
+             else if(choice==5)
+             group.opos++;
+             else
+             group.oneg++;
+
+         }
     }
     function Receive() public payable{
        received= msg.value;
