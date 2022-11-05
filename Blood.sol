d003aad85dbdf2c787c86fece4ec1f71a57e4c29
sanika-phadke <113808867+sanika-phadke@users.noreply.github.com>
2022-11-04 17:56:47 -0700

    Update Blood.sol

--- Blood.sol
+++ Blood.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.22 <=0.6.0;
 contract Blood{
     
-        address whoDonated;
+        address testingcenter;
         uint donated;
         struct BloodGrps{
             uint apos;
@@ -11,24 +11,60 @@ contract Blood{
             uint opos;
             uint oneg;
         }
+        /*mapping (address => bool) whoDonated;
+        function register() public payable{
+          whoDonated[msg.sender] = true;
+        } */
+    
         
-    
-    mapping(address => BloodGrps) bloodproj;
-    /*function whoDonated() public{
-        if(alreadyUser[msg.sender]==true)
-        //function 
-    }*/
-    
-        uint ReceiverId;
         uint  received;
+        uint age;
+        bool smoke;
+        bool drink;
+        uint bmi;
+        bool healthrisks;
+        bool tattoo;
     
     function Donate() public payable{
         donated=msg.value;
+        DonorValidity(age,smoke,drink,bmi,healthrisks,tattoo);
 
     }
-    function availableBloodGrps(uint choice) public 
+
+    modifier onlyTestingCenter{
+        require(msg.sender==testingcenter);
+        _;
+        
+
+    }
+    
+
+    function DonorValidity(uint userAge,bool userSmoke,bool userDrink,uint userBmi,bool userhealthrisks,bool usertattoo) public returns(bool) {
+        age=userAge;
+        smoke=userSmoke;
+        drink=userDrink;
+        bmi=userBmi;
+        healthrisks=userhealthrisks;
+        tattoo=usertattoo;
+        
+        uint flag=0;
+        if(age>=18)
+        flag=1;
+        if(smoke==false)
+        flag=1;
+        if(drink==false)
+        flag=1;
+        if(tattoo==false)
+        flag=1;
+        if(bmi>=18 && bmi<=24)
+        flag=1;
+        if(healthrisks==false)
+        flag=1;
+        return true;
+    }
+    /*function availableBloodGrps(string ) public 
     {
-          var group= bloodproj[msg.sender]; /*giving error*/
+          
          if(choice>=1 && choice<=6)
          {
              if(choice==1)
@@ -45,7 +81,7 @@ contract Blood{
              group.oneg++;
 
          }
-    }
+    } */
     function Receive() public payable{
        received= msg.value;
     }
